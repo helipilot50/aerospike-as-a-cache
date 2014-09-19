32a6d420549a2d2ece817e2dd4e5060d88a0fdbf
Peter Milne <helipilot50@gmail.com>
2014-09-19 14:33:14 -0500

    fixed log4j issue

--- src/main/java/com/aerospike/example/cache/AsACache.java
+++ src/main/java/com/aerospike/example/cache/AsACache.java
@@ -8,7 +8,6 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
